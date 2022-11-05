e57fbfcfb6d1392681452b074a395027344179a3
Penny Nichols <106450742+PennyNichols@users.noreply.github.com>
2022-11-05 08:35:35 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+// Dependencies :
+// bootstrap
+// react-bootstrap 
+// react-icons 
+// react-toastify 
+// firebase 
+
+
+// .env starter:
+// .env variable assignment setup:
+// REACT_APP_apiKey=
+// REACT_APP_authDomain=
+// REACT_APP_projectId=
+// REACT_APP_storageBucket=
+// REACT_APP_messagingSenderId=
+// REACT_APP_appId=
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
